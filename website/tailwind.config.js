6528840fc6a81f3777385e9cc55e9167a64e34b9
vitorecomp <vitor.ecomp@gmail.com>
2023-03-06 15:30:44 -0300

    feat: build the section structures of the site, and the footer

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{pug,html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    // require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
