e8feec8014891ccf60a01cc2b635df98e137e619
vitorecomp <vitor.ecomp@gmail.com>
2023-03-21 19:28:36 -0300

    feat: fix the deploy

--- reverse-proxy/deploy.sh
+++ reverse-proxy/deploy.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-echo "is working"
+home_path=$1
+
+echo "Copying files to the sites-avaiable"
+
+rm /etc/nginx/sites-available/*
+cp *.dev /etc/nginx/sites-available/
+
+echo "Creating the symbolic links"
+
+rm -f /etc/nginx/sites-enabled/*
+
+for file in `ls *.dev`; do
+    echo "creating the symbolic link of $file"
+    ln -s /etc/nginx/sites-available/$file /etc/nginx/sites-enabled/$file
+done
+
+echo "working on the certbot"
+
+mkdir -p $home_path/logs
+mkdir -p $home_path/cert-bot
+mkdir -p $home_path/ssl-certs
+
+for file in `ls *.dev`; do
+    echo "add the certification to $file"
+done
+
+
+
+
+
